894dad4be8624fcf85a5a0cadf6734fdc2861d02
mdelvecchio <mdelvecchio3119@gmail.com>
2023-09-20 20:31:44 -0400

    making the athlete model

---
+++ athletes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Athlete(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    sport = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    team = models.CharField(max_length=100)
+    year = models.IntegerField()
