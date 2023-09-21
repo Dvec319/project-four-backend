2b49e389012b84d083a542a6929443d4b9c520ba
mdelvecchio <mdelvecchio3119@gmail.com>
2023-09-20 20:51:40 -0400

    setting up athlete viewset

--- athletes/views.py
+++ athletes/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Athlete
+from .serializers import AthleteSerializer
 
 # Create your views here.
+
+class AthleteViewSet(viewsets.ModelViewSet):
+    queryset = Athlete.objects.all()
+    serializer_class = AthleteSerializer
+    permission_classes = [permissions.AllowAny]
